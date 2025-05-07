e837dd80d6f2202e845140bbd4f4bc7b08a8e052
TunaHoang31 <312hoang312@gmail.com>
2025-05-07 21:24:28 +0700

    h update

--- FitnessWeb/FitnessWeb/obj/Debug/net8.0/FitnessWeb.AssemblyInfo.cs
+++ FitnessWeb/FitnessWeb/obj/Debug/net8.0/FitnessWeb.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FitnessWeb")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0bd1412fcb0db15ad269a1c8550c38845561fba7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e16b81443b004702a47b2d3f463562b4b0f1de76")]
 [assembly: System.Reflection.AssemblyProductAttribute("FitnessWeb")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FitnessWeb")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
